f790ccada9562e9b652376723ad639ca547690c9
manjeete <manjeetkr2017@gmail.com>
2024-10-22 19:18:53 +0530

    login api issue fixed

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -7,7 +7,7 @@ const authValidation = require("../../validations/auth.validation");
 
 const { authController } = require("../../controllers");
 
-router.post("/login", authController.loginUser);
+router.post("/login", firebaseAuth(), authController.loginUser);
 router.post("/register", validate(authValidation.register), firebaseAuth(), authController.registerUser);
 
 router.post("/generate-token/:uid", generateToken);
